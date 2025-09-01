2a1ebddc81e2d6acfde7eddc7b7480d1d911e360
jaemin <jaemin4033@gmail.com>
2025-09-01 16:03:39 +0900

    add ErrorBoundary

---
+++ src/app/Error.tsx
@@ -0,0 +1,8 @@
+export default function Error() {
+  return (
+    <main style={{ display: "flex", flexDirection: "column" }}>
+      <h2>Error Page</h2>
+      <p>someting wrong...</p>
+    </main>
+  );
+}
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createBrowserRouter } from "react-router";
 import App from "./App";
+import Error from "./Error";
 const SampleRoutes = () =>
   import("../features/sample/routes").then((m) => m.default);
 
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     Component: App,
+    ErrorBoundary: Error,
     children: [...(await SampleRoutes())],
   },
 ]);
