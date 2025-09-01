f2b201b526af4d0585be0de56a60160339eedec9
jaemin <jaemin4033@gmail.com>
2025-09-01 16:43:10 +0900

    init tailwind

--- src/app/Error.tsx
+++ src/app/Error.tsx
@@ -1,8 +1,13 @@
+import { NavLink } from "react-router";
+
 export default function Error() {
   return (
-    <main style={{ display: "flex", flexDirection: "column" }}>
-      <h2>Error Page</h2>
+    <main className="flex flex-col gap-2 items-center justify-center">
+      <h2 className="text-6xl">Error Page</h2>
       <p>someting wrong...</p>
+      <NavLink to="/" className="button bg-amber-900 w-fit rounded-xl">
+        Return to Home
+      </NavLink>
     </main>
   );
 }
--- src/shared/components/Nav.tsx
+++ src/shared/components/Nav.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className="flex flex-col bg-[#373737] py-10 px-2 gap-1">
       <NavLink
         to="/"
         className={({ isActive, isPending, isTransitioning }) =>
@@ -10,12 +10,14 @@ export default function Nav() {
             isActive ? "active" : "",
             isPending ? "pending" : "",
             isTransitioning ? "transitioning" : "",
-          ].join(" ")
+          ].join(" ") + "button"
         }
       >
         Welcome
       </NavLink>
-      <NavLink to="guest">Greeting</NavLink>
+      <NavLink to="guest" className={"button"}>
+        Greeting
+      </NavLink>
     </nav>
   );
 }
