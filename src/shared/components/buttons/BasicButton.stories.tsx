75ec63836c7f95b50f54788bbadf865d208432fb
jaemin <jaemin4033@gmail.com>
2025-09-18 17:23:44 +0900

    remove sample

--- src/shared/components/buttons/BasicButton.stories.tsx
+++ src/shared/components/buttons/BasicButton.stories.tsx
@@ -2,6 +2,14 @@ import { fn } from "storybook/internal/test";
 import { BasicButton } from "./BasicButton";
 import { Meta, StoryObj } from "@storybook/react-vite";
 
+/*
+ Storybook 메타데이터 (필수)
+ - title: 스토리북 사이드바에서 보여질 이름/경로
+ - component: 문서화할 실제 컴포넌트
+ - tags: autodocs를 활성화하면 자동 문서화됨
+ - argTypes: 컨트롤러(Controls 패널)에서 조정 가능한 props 정의
+ - args: 모든 스토리에서 공통적으로 적용할 기본 prop 값
+ */
 const meta = {
   title: "Shared/Buttons/BasicButton",
   component: BasicButton,
